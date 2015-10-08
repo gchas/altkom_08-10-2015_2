408bbcc33ef09281e3e052ac418745ef2315670a
Grzegorz Chaś <grzegorz.chas01@gmail.com>
2015-10-08 14:41:50 +0200

    Logger

---
+++ src/pl/altkom/LoggerTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.altkom;
+
+import org.apache.log4j.Logger;
+
+public class LoggerTest {
+    
+    static Logger logger = Logger.getLogger(LoggerTest.class);
+    
+    public static void main(String[] args) {
+        
+        logger.debug("zalogowalem");
+    }
+}
